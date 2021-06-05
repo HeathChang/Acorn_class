299fa88867089367587801b098c54a0a6885cd82
HeathChang <jannhyunsoo@gmail.com>
2021-06-05 16:37:55 +0900

    210605_javascript

---
+++ WORKSPACE(JavaScript)/01HTML_CSS/WebContent/js/script.js
@@ -0,0 +1,6 @@
+document.write("Hello World");
+		document.write("<h1> Hello World </h1>");
+	console.log("HelloWorld============");
+	document.write("Hello World");
+	document.write("Hello World");
+	document.write("<h1> Hello World </h1>");
